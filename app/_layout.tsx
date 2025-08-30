93cd2c2cd7a4a7a65b376e3282804fe82a22ffc7
Michael Hurhangee <m.hurhangee@me.com>
2025-08-30 15:42:27 +0100

    gluestack landing page

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,5 +4,9 @@ import { GluestackUIProvider } from '@/components/ui/gluestack-ui-provider';
 import '@/global.css';
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <GluestackUIProvider mode="light">
+      <Stack />
+    </GluestackUIProvider>
+  );
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,44 @@
-import { Button, ButtonText } from '@/components/ui/button'
+import { Button, ButtonText } from '@/components/ui/button';
+import { Center } from '@/components/ui/center';
+import { HStack } from '@/components/ui/hstack';
+import { Text } from '@/components/ui/text';
+import { VStack } from '@/components/ui/vstack';
+import React from 'react';
+import { SafeAreaView } from 'react-native';
 
-export default function App() {
+export default function WelcomeSplash() {
+  const handleGetStarted = () => {
+    // Add your navigation logic here
+    console.log('Get Started pressed');
+  };
+
   return (
-    <Button>
-      <ButtonText>Get Started</ButtonText>
-    </Button>
-  )
+    <SafeAreaView className="flex-1">
+      <Center className="flex-1 px-6">
+        <VStack space="xl" className="items-center">
+          <Text className="text-4xl font-semibold text-center text-typography-900">
+            Welcome!
+          </Text>
+          <HStack space="lg">
+          <Button 
+            variant="solid"
+            action="secondary"
+            onPress={handleGetStarted}
+            className="mt-4"
+          >
+            <ButtonText>Get Started</ButtonText>
+          </Button>
+          <Button 
+            variant="solid"
+            action="primary"
+            onPress={handleGetStarted}
+            className="mt-4"
+          >
+            <ButtonText>Learn more</ButtonText>
+          </Button>
+          </HStack>
+        </VStack>
+      </Center>
+    </SafeAreaView>
+  );
 }
