0b6c9aa66c9fd74f7358f1068bac75ab8870fc0a
Michael Hurhangee <m.hurhangee@me.com>
2025-08-30 20:00:51 +0100

    add custom fonts, icon component, improved splash, external links

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,7 +3,36 @@ import { Stack } from "expo-router";
 import { GluestackUIProvider } from '@/components/ui/gluestack-ui-provider';
 import '@/global.css';
 
+import { useEffect } from "react";
+import { useCustomFonts } from '../hooks/useCustomFonts';
+
+import * as SplashScreen from 'expo-splash-screen';
+
+export {
+  // Catch any errors thrown by the Layout component.
+  ErrorBoundary
+} from 'expo-router';
+
+// Prevent the splash screen from auto-hiding
+SplashScreen.preventAutoHideAsync();
+
 export default function RootLayout() {
+  const [loaded, error] = useCustomFonts();
+
+  useEffect(() => {
+    if (error) throw error;
+  }, [error]);
+
+  useEffect(() => {
+    if (loaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded]);
+
+  if (!loaded) {
+    return null; // Keep showing native splash screen while fonts load
+  }
+
   return (
     <GluestackUIProvider mode="light">
       <Stack />
--- app/index.tsx
+++ app/index.tsx
@@ -1,44 +1,71 @@
+import { Icon } from '@/components/icon';
+import { Box } from '@/components/ui/box';
 import { Button, ButtonText } from '@/components/ui/button';
 import { Center } from '@/components/ui/center';
 import { HStack } from '@/components/ui/hstack';
+import { Link } from '@/components/ui/link';
 import { Text } from '@/components/ui/text';
 import { VStack } from '@/components/ui/vstack';
-import React from 'react';
 import { SafeAreaView } from 'react-native';
 
 export default function WelcomeSplash() {
   const handleGetStarted = () => {
-    // Add your navigation logic here
     console.log('Get Started pressed');
   };
 
   return (
     <SafeAreaView className="flex-1">
-      <Center className="flex-1 px-6">
-        <VStack space="xl" className="items-center">
-          <Text className="text-4xl font-semibold text-center text-typography-900">
-            Welcome!
-          </Text>
-          <HStack space="lg">
-          <Button 
-            variant="solid"
-            action="secondary"
-            onPress={handleGetStarted}
-            className="mt-4"
-          >
-            <ButtonText>Get Started</ButtonText>
-          </Button>
-          <Button 
-            variant="solid"
-            action="primary"
-            onPress={handleGetStarted}
-            className="mt-4"
-          >
-            <ButtonText>Learn more</ButtonText>
-          </Button>
-          </HStack>
-        </VStack>
-      </Center>
+      <Box className="flex-1 px-6 justify-between">
+        
+        {/* Main content centered */}
+        <Center className="flex-1">
+          <VStack space="xl" className="items-center">
+            <Text className="text-4xl font-heading font-bold text-center text-typography-900">
+              Welcome!
+            </Text>
+            <Text className="text-lg font-body font-semibold text-center text-typography-500">
+              This is a demo expo and gluestack app.
+            </Text>
+            <HStack space="lg">
+              <Button
+                variant="solid"
+                action="primary"
+                onPress={handleGetStarted}
+                className="mt-4"
+              >
+                <Icon name="rocket" size={12} variant="inverse" className="mr-1" />
+                <ButtonText>Get Started</ButtonText>
+              </Button>
+              <Button
+                variant="solid"
+                action="secondary"
+                onPress={handleGetStarted}
+                className="mt-4"
+              >
+                <Icon name="book" size={12} className="mr-1" />
+                <ButtonText>Learn more</ButtonText>
+              </Button>
+            </HStack>
+          </VStack>
+        </Center>
+
+        {/* Built by section at bottom */}
+        <Box className="pb-4 items-center">
+          <VStack className="items-center">
+            <Text className="text-sm font-mono text-typography-700">
+              Built by m.hurhangee@me.com
+            </Text>
+            <HStack className="gap-4 mt-4">
+              <Link href="https://github.com/mhurhangee/expo-ai-glue">
+                <Icon name="github" variant="muted" />
+              </Link>
+              <Link href="mailto:m.hurhangee@me.com">
+              <Icon name="envelope" variant="muted" />
+              </Link>
+            </HStack>
+          </VStack>
+        </Box>
+      </Box>
     </SafeAreaView>
   );
 }
